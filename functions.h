9ce7e91afe18f522065d8e2ce89606124e158382
eheard00 <evanheard2000@gmail.com>
2024-06-11 20:42:31 -0500

    Assignment 1

---
+++ functions.h
@@ -0,0 +1,10 @@
+/*******************************************************************************
+    Function Declarations
+*******************************************************************************/
+int search(int array[], int reccnt, int searchvalue);
+
+int modify(int array[], int index, int new_value, int reccnt);
+
+void remove(int array[], int index, int reccnt);
+
+int add(int array[], int reccnt, int new_value);
